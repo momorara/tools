10606251af83e10e1d9f147ca4c1ccaf37e9cf09
T.kawabata <52056772+momorara@users.noreply.github.com>
2022-12-05 14:53:28 +0900

    Update cds.py

--- cds.py
+++ cds.py
@@ -1,11 +1,11 @@
 
 
 
-def cds():
+def Cds_stat():
 	# 0.5秒ごとに２０回cdsの状態を読み取り、その合計を計算
 	cds_total = 0
 	for i in range(20):
-		cds_total =  cds_total + read_cds
+		cds_total =  cds_total + pi.read(Cds)
 		time.sleep(0.5)
 		
 	# その結果からoff、on、満水を判断する。
