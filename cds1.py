9fbcdef4cbed28b8f743c1dca8e16fe64942a3f9
T.kawabata <52056772+momorara@users.noreply.github.com>
2022-12-05 15:09:55 +0900

    Add files via upload

---
+++ cds1.py
@@ -0,0 +1,29 @@
+
+
+
+def cds():
+	# 0.5秒ごとに２０回cdsの状態を読み取り、その合計を計算
+	cds_total = 0
+	for i in range(20):
+		cds_total =  cds_total + read_cds
+		time.sleep(0.5)
+		
+	# その結果からoff、on、満水を判断する。
+	if cds_total == 0:
+		return 'off'
+	if cds_total > 18:
+		return 'on'
+	return '満水'
+	
+	
+def cds_stat():
+	# cds状態を確認
+	
+	on,offならその値を持って帰る。
+	
+	満水なら何もせずにここに留まるのも手だが
+	温度、湿度をambientに投げないといけないので
+	、どうしたものか
+	
+		
+
